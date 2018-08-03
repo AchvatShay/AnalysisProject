ccdab82c3a60b74dcd0d330723b66b0feb8a15db
AchvatShay <41129718+AchvatShay@users.noreply.github.com>
2018-08-03 15:15:41 +0300

    #1 add interface functions for Hadas

---
+++ MatlabAnalysis/getAllExperimentNeurons.m
@@ -0,0 +1,4 @@
+function [Neurons] = getAllExperimentNeurons(TPAFile)
+   % get all Neuron names for the TPA file as list
+   Neurons = 0;
+end
--- MatlabAnalysis/runAnalysis.m
+++ MatlabAnalysis/runAnalysis.m
@@ -1,9 +1,9 @@
-function [res] = runAnalysis(outputPath, xmlfile, BdaTpaList)
+function runAnalysis(outputPath, xmlfile, BdaTpaList, analysisName)
     mkNewFolder(outputPath);
 
     generalProperty = Experiment(xmlfile);
     [imagingData, BehaveData] = loadData(BdaTpaList, generalProperty);
-    analysis('Pca2D', outputPath, generalProperty, imagingData, BehaveData);
-    
-    res = 1;
+    for k = 1:length(analysisName)
+    analysis(analysisName{k}, outputPath, generalProperty, imagingData, BehaveData);
+    end
 end
