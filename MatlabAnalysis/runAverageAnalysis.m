8c8cb5c3de01c88a43887da6cd4fbf1425b74dee
hadasbenisty <hadas.benisty@gmail.com>
2018-08-27 01:42:41 +0300

    1. plotting much nicer the confusion matrix. 2. also plotting it in the averaged accuracy code

--- MatlabAnalysis/runAverageAnalysis.m
+++ MatlabAnalysis/runAverageAnalysis.m
@@ -1,9 +1,9 @@
-function runAverageAnalysis(outputPath, generalProperty, MatList, analysisName)
+function runAverageAnalysis(outputPath, generalProperty, MatList, analysisName, specificParams)
 
 for file_i = 1:length(MatList)
     analysisRes(file_i) = load(MatList{file_i});
 end
 
-feval([analysisName 'AverageAnalysis'], outputPath, generalProperty, analysisRes); 
+feval([analysisName 'AverageAnalysis'], outputPath, generalProperty, analysisRes, specificParams); 
 
 end
