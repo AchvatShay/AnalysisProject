8cd2dd32d9023f25064fb7daed05190c577ef9e3
hadasbenisty <hadas.benisty@gmail.com>
2018-12-13 15:51:40 -0500

    plot single neurons with empty hist

--- MatlabAnalysis/plotsinglNrnPerTrials.m
+++ MatlabAnalysis/plotsinglNrnPerTrials.m
@@ -10,8 +10,11 @@ c=colorbar;
 set(c,'Position',[0.9214    0.5048    0.0397    0.4238]);
 set(htop,'Position',[0.1300    0.5071    0.7750    0.4179]);
 set(gca, 'YTick', unique([1 get(gca, 'YTick')]))
-
-hmid=subplot(4,1,3);
+if ~isempty(histbehave)
+    hmid=subplot(4,1,3);
+else
+    hmid=subplot(2,1,2);
+end
 plot(t,mean(X(currnrnind,:,faillabels==chosenLabelval),3), 'LineWidth',3, 'Color','k');
 ylabel('Average','FontSize',10)
 set(gca, 'YLim', [mA MA]);
@@ -21,16 +24,20 @@ if length(ticks) > 3
 end
 placeToneTime(0, 2);
 set(gca, 'Box','off');
-set(gca, 'XTick',[]);
-set(hmid, 'Position', [0.1300    0.3291    0.7750    0.1066]);
+
 xlim([xlimmin,t(end)])
-xa=get(gca, 'XAxis');
-set(xa,'Visible', 'off')
-subplot(4,1,4);
-lh=plotBehaveHist(t, histbehave, 0, generalProperty.Events2plot);
-set(lh, 'Visible','off');
+
+if ~isempty(histbehave)
+    set(gca, 'XTick',[]);
+    set(hmid, 'Position', [0.1300    0.3291    0.7750    0.1066]);
+    xa=get(gca, 'XAxis');
+    set(xa,'Visible', 'off')
+    subplot(4,1,4);
+    lh=plotBehaveHist(t, histbehave, 0, generalProperty.Events2plot);
+    set(lh, 'Visible','off');
+    xlim([xlimmin,t(end)])
+end
 xlabel('Time [sec]','FontSize',10);
-xlim([xlimmin,t(end)])
 
 
 % mysave(gcf, fullfile(outputPath, [outputstr num2str(roiNames(currnrnind))]));
