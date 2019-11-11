ad2074e3324850c092bf0a8bc1c1b3727bc1181f
AchvatShay <shay.achvat@campus.technion.ac.il>
2019-11-11 08:42:09 +0200

    commit hadas code

--- MatlabAnalysis/getConfidenceInterval.m
+++ MatlabAnalysis/getConfidenceInterval.m
@@ -4,7 +4,7 @@ SEM = s/sqrt(n);               % Standard Error
 ts = tinv([ perc 1-perc],(n)-1);      % T-Score
 
 
-C = m+ts*SEM;
+C = bsxfun(@plus, ts'*SEM(:).',m(:)') ;
 if any(isnan(C))
 %     disp(1)
 end
