9dd9b5898f2506e2a84920a4935c603bb7775166
AchvatShay <shay.achvat@campus.technion.ac.il>
2020-03-09 09:39:25 +0200

    add std to trajCompareAnalysis.m

--- MatlabAnalysis/trajCompareAnalysis.m
+++ MatlabAnalysis/trajCompareAnalysis.m
@@ -2,41 +2,49 @@ function trajCompareAnalysis(outputPath, generalProperty, imagingData, BehaveDat
     trajAvarage = mean(BehaveData.traj.data, 3);
     
     t = linspace(0, generalProperty.Duration, size(BehaveData.traj.data,2));
-   
-    sum_front_zero = 0;
-    sum_side_zero = 0;
-    
-    sum_front_tone = 0;
-    sum_side_tone = 0;
     
     trajSize = size(BehaveData.traj.data, 3);
+    
+    sum_front_zero = zeros(1, trajSize);
+    sum_side_zero = zeros(1, trajSize);
+    
+    sum_front_tone = zeros(1, trajSize);
+    sum_side_tone = zeros(1, trajSize);
+    
     for currentTraj = 1:trajSize
         % Compare Start Trial zero time
         endT = 1;
-        sum_front_zero = sum_front_zero + emd_calc(trajAvarage(1:2, 1:(findClosestDouble(t, endT)))',...
+        sum_front_zero(currentTraj) = emd_calc(trajAvarage(1:2, 1:(findClosestDouble(t, endT)))',...
             BehaveData.traj.data(1:2, 1:(findClosestDouble(t, endT)), currentTraj)');
         
-        sum_side_zero = sum_side_zero + emd_calc(trajAvarage(3:4, 1:(findClosestDouble(t, endT)))',...
+        sum_side_zero(currentTraj) = emd_calc(trajAvarage(3:4, 1:(findClosestDouble(t, endT)))',...
             BehaveData.traj.data(3:4, 1:(findClosestDouble(t, endT)), currentTraj)');
    
         
         % Compare Start Trial Tone time
         startTime = findClosestDouble(t, generalProperty.ToneTime - 0.5);
         endT = generalProperty.ToneTime + 0.5;
-        sum_front_tone = sum_front_tone + emd_calc(trajAvarage(1:2, startTime:(findClosestDouble(t, endT)))',...
+        sum_front_tone(currentTraj) = emd_calc(trajAvarage(1:2, startTime:(findClosestDouble(t, endT)))',...
             BehaveData.traj.data(1:2, startTime:(findClosestDouble(t, endT)), currentTraj)');
         
-        sum_side_tone = sum_side_tone + emd_calc(trajAvarage(3:4, startTime:(findClosestDouble(t, endT)))',...
+        sum_side_tone(currentTraj) = emd_calc(trajAvarage(3:4, startTime:(findClosestDouble(t, endT)))',...
             BehaveData.traj.data(3:4, startTime:(findClosestDouble(t, endT)), currentTraj)');
    
     end
     
-    avg_front_zero = sum_front_zero ./ trajSize;
-    avg_side_zero = sum_side_zero ./ trajSize;
-    avg_front_tone = sum_front_tone ./ trajSize;
-    avg_side_tone = sum_side_tone ./ trajSize;
+    avg_front_zero = mean(sum_front_zero);
+    avg_side_zero = mean(sum_side_zero);
+    avg_front_tone = mean(sum_front_tone);
+    avg_side_tone = mean(sum_side_tone);
     
-    tableSave = table(avg_front_zero, avg_side_zero, avg_front_tone, avg_side_tone, 'VariableNames',  {'Front_Zero_PeriTime', 'Side_Zero_PeriTime', 'Front_Tone_PeriTime', 'Side_Tone_PeriTime'});
+    std_front_zero = std(sum_front_zero);
+    std_side_zero = std(sum_side_zero);
+    std_front_tone = std(sum_front_tone);
+    std_side_tone = std(sum_side_tone);
+    
+    tableSave = table(avg_front_zero, std_front_zero, avg_side_zero, std_side_zero, avg_front_tone, std_front_tone, avg_side_tone, std_side_tone, ...
+        'VariableNames',  {'Front_Zero_PeriTime_avg', 'Front_Zero_PeriTime_std', 'Side_Zero_PeriTime_avg',...
+        'Side_Zero_PeriTime_std', 'Front_Tone_PeriTime_avg', 'Front_Tone_PeriTime_std', 'Side_Tone_PeriTime_avg' , 'Side_Tone_PeriTime_std'});
     writetable(tableSave,[outputPath '\compareResults.xlsx'],'Sheet',1,'Range','D1')
 end
 
